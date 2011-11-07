01df9f49e1bea52b6c92c9c79eb8c74e438c3339
SeanWeber <bluesmew@gmail.com>
2011-11-07 16:32:34 -0500

    Added user input and country selection

--- system.py
+++ system.py
@@ -1,9 +1,19 @@
-import zipfile, io
+import zipfile, io, re
 
-archive = zipfile.ZipFile("countries.zip", "r") #access a zip archive
+def main():
+  country = raw_input("Enter a Country:")
+  file = open("countryList.txt", "r")
+  for line in file:
+    if country in line: 
+      countryPage = line[:2] + ".html"
+  countrySearch(countryPage)
 
-file = archive.open("aa.html", "r")
+def countrySearch(page):
+  archive = zipfile.ZipFile("countries.zip", "r") #access a zip archive
+  file = archive.open(page, "r")
+  for line in file:  #searches the file containing a string and outputs it
+    if "Background" in line: 
+      print line
 
-for line in file:  #searches the file containing a string and outputs it
-  if "People and Society" in line: 
-    print line
+if __name__ == '__main__':
+  main()
